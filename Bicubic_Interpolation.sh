f644c41a0380bde5a5eae5721efdb5e1915aedf5
sasthana <shivanshi.asthana@unil.ch>
2025-05-17 16:19:10 +0200

    Put in relative paths for security

--- Bicubic_Interpolation.sh
+++ Bicubic_Interpolation.sh
@@ -7,12 +7,13 @@
 #SBATCH --mem=32G
 #SBATCH --time=3-00:00:00
 
-cd /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling_Data_Prep
+# Load paths from ignored config
+source ../Variable_Config_Scripts/env_filepaths_bicubic.sh
+
+cd "$BASE_DIR"
 module load cdo
 
-# Perform bicubic interpolation using the saved grid files
-cdo remapbic,wet_days_filtered_files/Targets/RhiresD_wet_1971_2023.nc wet_days_filtered_files/Features/rhiresd_wet_coarsened.nc Bicubic_Baselines/bicubic_rhiresd.nc
-cdo remapbic,wet_days_filtered_files/Targets/TabsD_wet_1971_2023.nc  wet_days_filtered_files/Features/tabsd_wet_coarsened.nc   Bicubic_Baselines/bicubic_tabsd.nc
-cdo remapbic,wet_days_filtered_files/Targets/TmaxD_wet_1971_2023.nc   wet_days_filtered_files/Features/tmaxd_wet_coarsened.nc   Bicubic_Baselines/bicubic_tmax.nc
-cdo remapbic,wet_days_filtered_files/Targets/TminD_wet_1971_2023.nc   wet_days_filtered_files/Features/tmind_wet_coarsened.nc   Bicubic_Baselines/bicubic_tmind.nc
-
+cdo remapbic,"$GRID_DIR/RhiresD_wet_1971_2023.nc" "$FEATURE_DIR/rhiresd_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_rhiresd.nc"
+cdo remapbic,"$GRID_DIR/TabsD_wet_1971_2023.nc" "$FEATURE_DIR/tabsd_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_tabsd.nc"
+cdo remapbic,"$GRID_DIR/TmaxD_wet_1971_2023.nc" "$FEATURE_DIR/tmaxd_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_tmax.nc"
+cdo remapbic,"$GRID_DIR/TminD_wet_1971_2023.nc" "$FEATURE_DIR/tmind_wet_coarsened.nc" "$OUTPUT_DIR/bicubic_tmind.nc"
--- Bilinear_Interpolation.sh
+++ Bilinear_Interpolation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=bicubic
+#SBATCH --job-name=bilinear
 #SBATCH --output=coarsen_%j.out
 #SBATCH --error=coarsen_%j.err
 #SBATCH --ntasks=1
@@ -7,13 +7,14 @@
 #SBATCH --mem=32G
 #SBATCH --time=3-00:00:00
 
-cd /work/FAC/FGSE/IDYST/tbeucler/downscaling/sasthana/Downscaling/Downscaling_Data_Prep
+cd "$BASE_DIR"
 module load cdo
 
+
 # Perform bicubic interpolation using the saved grid files
-cdo remapbil,wet_days_filtered_files/Targets/RhiresD_wet_1971_2023.nc wet_days_filtered_files/Features/rhiresd_wet_coarsened.nc Bilinear_Baselines/bilinear_rhiresd.nc
-cdo remapbil,wet_days_filtered_files/Targets/TabsD_wet_1971_2023.nc  wet_days_filtered_files/Features/tabsd_wet_coarsened.nc   Bilinear_Baselines/bilinear_tabsd.nc
-cdo remapbil,wet_days_filtered_files/Targets/TmaxD_wet_1971_2023.nc   wet_days_filtered_files/Features/tmaxd_wet_coarsened.nc   Bilinear_Baselines/bilinear_tmax.nc
-cdo remapbil,wet_days_filtered_files/Targets/TminD_wet_1971_2023.nc   wet_days_filtered_files/Features/tmind_wet_coarsened.nc   Bilinear_Baselines/bilinear_tmind.nc
+cdo remapbil,"$GRID_DIR/RhiresD_wet_1971_2023.nc" "$FEATURE_DIR/rhiresd_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_rhiresd.nc"
+cdo remapbil,"$GRID_DIR/TabsD_wet_1971_2023.nc" "$FEATURE_DIR/tabsd_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_tabsd.nc"
+cdo remapbil,"$GRID_DIR/TmaxD_wet_1971_2023.nc" "$FEATURE_DIR/tmaxd_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_tmax.nc"
+cdo remapbil,"$GRID_DIR/TminD_wet_1971_2023.nc" "$FEATURE_DIR/tmind_wet_coarsened.nc" "$OUTPUT_DIR/bilinear_tmind.nc"
 
 
